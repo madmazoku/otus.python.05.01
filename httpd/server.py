723001ad24dde2b3d55705b24eab85190bc52e9c
Unknown <madmazoku@gmail.com>
2018-07-13 20:49:28 +0300

    fix for tests

--- httpd/server.py
+++ httpd/server.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
 import datetime
 import pathlib
 import socket
 import select
+import logging
+import re
 
-IO_BUF_SIZE = 4# * 1<<10
-IO_BUF_MAX_SIZE = 10 * 1<<20
-CLIENT_CLOSE_FLAGS = select.EPOLLERR|select.EPOLLHUP|select.EPOLLRDHUP
+IO_BUF_SIZE = 4 * 1 << 10
+CLIENT_CLOSE_FLAGS = select.EPOLLERR | select.EPOLLHUP | select.EPOLLRDHUP
 
 OK = 200
 FORBIDDEN = 403
@@ -24,7 +26,7 @@ STATUS = {
 }
 
 CONTENT_TYPE = {
-    '.html': 'text/html; charset=UTF-8',
+    '.html': 'text/html',
     '.css': 'text/css',
     '.js': 'application/javascript',
     '.jpg': 'image/jpeg',
@@ -34,99 +36,148 @@ CONTENT_TYPE = {
     '.swf': 'application/x-shockwave-flash'
 }
 
-class RequestRead:
+
+class Actor(object):
     def __init__(self, server, socket):
         self.server = server
         self.socket = socket
+        self.time = time.time()
+
+    def act(self, event):
+        pass
+
+    def close(self, event):
+        self.socket.close()
+
+    def process_time(self):
+        return (time.time() - self.time) * 1000
+
+
+class RequestRead(Actor):
+    def __init__(self, server, socket):
+        super().__init__(server, socket)
+        self.status = b''
         self.buffer = b''
         self.server.register(self, self.socket.fileno(), select.EPOLLIN)
 
+    def close(self, event):
+        logging.info('CLOSE\t%d; read; buffer: %s; time: %.3f', self.socket.fileno(), self.buffer, self.process_time())
+        super().close(event)
 
     def act(self, event):
         if not event & select.EPOLLIN:
             return
+
         recieved = self.socket.recv(IO_BUF_SIZE)
-        if recieved:
+        if len(recieved) == 0:
+            logging.info('CLOSE\t%d, premature end of read', self.socket.fileno())
+            self.socket.close()
+            self.server.unregister(self.socket.fileno())
+        else:
             self.buffer += recieved
-            end_of_headers = self.buffer.find(b'\r\n\r\n')
-            if end_of_headers != -1:
-                lines = self.buffer[:end_of_headers].split(b'\r\n')
-                (method, uri, version) = lines[0].split(b'\x20')
-                self.buffer = self.buffer[end_of_headers+4:]
-                RequestWrite(self.server, self.socket, method, uri)
+
+        end_of_headers = self.buffer.find(b'\r\n\r\n')
+        if end_of_headers != -1:
+            lines = self.buffer[:end_of_headers].split(b'\r\n')
+            self.status = lines.pop(0)
+            self.buffer = b''
+            RequestWrite(self)
 
 
-class RequestWrite:
-    def __init__(self, server, socket, method, uri):
-        self.server = server
-        self.socket = socket
+class RequestWrite(Actor):
+    def __init__(self, reader):
+        super().__init__(reader.server, reader.socket)
         self.file = None
+        self.time = reader.time
+        self.status = reader.status
+        self.page = None
+
+        (self.method, self.uri, self.version) = self.status.split(b'\x20')
 
         headers = ''
-        headers += datetime.datetime.utcnow().strftime('Date: %a, %d %b %Y %H:%M:%S UTC\r\n')
-        headers += 'Server: httpd.py\r\n'
-        headers += 'Connection: close\r\n'
 
-        print('method:', method)
-        status = INTERNAL_ERROR
-        if method in {b'GET', b'HEAD'}:
-            paths = uri.decode('ascii').split('/')
-            print('uri:', paths)
-            page = self.server.root.joinpath(*paths).resolve()
-            if self.server.root not in page.parents:
-                status = FORBIDDEN
-            else:
-                if page.is_dir():
-                    page = page / 'index.html'
-                if page.is_file():
-                    if method == b'GET':
-                        self.file = open(str(page), 'rb')
-                    print('File:', self.file)
-                    headers += 'Content-Length: {:d}\r\n'.format(page.stat().st_size)
-                    if page.suffix.lower() in CONTENT_TYPE:
-                        headers += 'Content-Type: {:s}\r\n'.format(CONTENT_TYPE[page.suffix.lower()])
-                    status = OK
+        self.code = INTERNAL_ERROR
+        try:
+            if self.method in {b'GET', b'HEAD'}:
+                match = re.search(rb'[\?#]', self.uri)
+                request_path = self.uri[:len(self.uri) if match is None else match.start()]
+                parts = []
+                for part in request_path.split(b'/'):
+                    part = re.sub(rb'\+', b' ', part)
+                    part = re.sub(rb'%([0-9a-fA-F]{2})', lambda m: bytes.fromhex(m.group(1).decode('ascii')), part)
+                    parts.append(part.decode('utf-8'))
+                self.page = self.server.root.joinpath(*parts).resolve()
+
+                if self.server.root == self.page or self.server.root in self.page.parents:
+                    if self.page.is_dir():
+                        self.page = self.page / 'index.html'
+                    if self.page.is_file():
+                        if self.method == b'GET':
+                            self.file = open(str(self.page), 'rb')
+                        headers += 'Content-Length: {:d}\r\n'.format(self.page.stat().st_size)
+                        if self.page.suffix.lower() in CONTENT_TYPE:
+                            headers += 'Content-Type: {:s}\r\n'.format(CONTENT_TYPE[self.page.suffix.lower()])
+                        self.code = OK
+                    else:
+                        self.code = NOT_FOUND
                 else:
-                    status = NOT_FOUND
-        else:
-            status = METHOD_NOT_ALLOWED
+                    self.code = FORBIDDEN
+            else:
+                self.code = METHOD_NOT_ALLOWED
+        except Exception as e:
+            logging.error('Internal error: %s', e)
 
-        status_line = 'HTTP/1.1 {:d} {:s}\r\n'.format(status, STATUS[status])
+        buffer = 'HTTP/1.1 {:d} {:s}\r\n'.format(self.code, STATUS[self.code])
+        buffer += datetime.datetime.utcnow().strftime('Date: %a, %d %b %Y %H:%M:%S UTC\r\n')
+        buffer += 'Server: httpd.py\r\n'
+        buffer += 'Connection: close\r\n'
+        buffer += headers + '\r\n'
 
-        buffer = status_line + headers + '\r\n'
-        print('Sent:', buffer)
         self.buffer = buffer.encode('ascii')
         self.server.register(self, self.socket.fileno(), select.EPOLLOUT)
 
+    def close(self, event):
+        logging.info('CLOSE\t%d; write; request: %s; time: %.3f', self.socket.fileno(), self.status,
+                     self.process_time())
+        super().close(event)
+
+    def finish(self):
+        logging.info('FINISH\t%d; request: %s; page: %s; code: %d; time: %.3f', self.socket.fileno(), self.status,
+                     self.page, self.code, self.process_time())
+        self.server.unregister(self.socket.fileno())
 
     def act(self, event):
         if not event & select.EPOLLOUT:
             return
 
-        sent = self.socket.send(self.buffer[:IO_BUF_SIZE])
+        try:
+            sent = self.socket.send(self.buffer[:IO_BUF_SIZE])
+        except (ConnectionResetError, BrokenPipeError):
+            self.finish()
+            return
+
         self.buffer = self.buffer[sent:]
 
-        if len(self.buffer) == 0:
-            if self.file:
-                buffer = self.file.read(IO_BUF_SIZE)
-                if buffer is None:
-                    self.server.unregister(self.socket.fileno())
-                    self.socket.shutdown(socket.SHUT_RDWR)
-                else:
-                    self.buffer = buffer
+        if len(self.buffer) == 0 and self.file is not None:
+            buffer = self.file.read(IO_BUF_SIZE)
+            if len(buffer) == 0:
+                self.file.close()
+                self.file = None
             else:
-                self.server.unregister(self.socket.fileno())
-                self.socket.shutdown(socket.SHUT_RDWR)
+                self.buffer = buffer
+
+        if len(self.buffer) == 0 and self.file is None:
+            self.finish()
+            self.socket.shutdown(socket.SHUT_RDWR)
 
 
 class HTTPServer:
-    def __init__(self, root = '.', host = 'localhost', port = 8080):
+    def __init__(self, root='.', host='localhost', port=8080):
         self.root = pathlib.Path(root).resolve()
         self.host = host
         self.port = port
         self.socket = None
         self.clients = {}
-        self.poll = select.epoll()
 
     def register(self, actor, fileno, flags):
         if fileno in self.clients:
@@ -136,6 +187,8 @@ class HTTPServer:
         self.clients[fileno] = actor
 
     def unregister(self, fileno):
+        if fileno == -1:
+            return
         if fileno in self.clients:
             del self.clients[fileno]
         self.poll.unregister(fileno)
@@ -143,36 +196,58 @@ class HTTPServer:
     def listen(self):
         if self.socket is not None:
             self.close()
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM|socket.SOCK_NONBLOCK)
-        print('bind to:',self.host, self.port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM | socket.SOCK_NONBLOCK)
         self.socket.bind((self.host, self.port))
-        self.socket.listen(0)
+        self.socket.listen()
+        logging.info('LISTEN\t%d', self.socket.fileno())
 
     def close(self):
+        for fileno, actor in self.clients.items():
+            if actor.socket.fileno() != -1:
+                self.poll.unregister(actor.socket.fileno())
+            actor.close(0)
+        self.clients = {}
+
         self.socket.close()
+        if self.socket.fileno() != -1:
+            self.poll.unregister(self.socket.fileno())
         self.socket = None
 
-    def wait(self, timeout = None):
+        self.poll.close()
+        self.poll = None
+
+        logging.info('Connection socket closed')
+
+    def wait(self, timeout=None):
+        self.poll = select.epoll()
         self.poll.register(self.socket.fileno(), select.EPOLLIN)
-        print('register main socket', self.socket.fileno())
         while True:
             events = self.poll.poll(5)
+            logging.info('TICK\t%s', events)
             for fileno, event in events:
                 if fileno == self.socket.fileno():
-                    print("connection")
-                    try:
-                        client_socket, addr = self.socket.accept()
-                        RequestRead(self, client_socket)
-                    except Exception as e:
-                        print("Exception: ", e)
+                    client_socket, addr = self.socket.accept()
+                    logging.info("ADD\t%d; Pending connections: %d", client_socket.fileno(), len(self.clients))
+                    RequestRead(self, client_socket)
                 elif fileno in self.clients:
                     if event & CLIENT_CLOSE_FLAGS:
-                        print('client connection lost', fileno, event)
                         self.poll.unregister(fileno)
-                        self.clients[fileno].socket.close()
+                        self.clients[fileno].close(event)
                         del self.clients[fileno]
                     else:
                         self.clients[fileno].act(event)
                 else:
-                    print('unknown client connection closed', fileno, event)
+                    logging.info("CLOSE\t%d; Remote; Unknown", fileno)
                     self.poll.unregister(fileno)
+
+            remove = []
+            actors = []
+            for fileno, actor in self.clients.items():
+                actors.append('({:d}, {:.3f})'.format(actor.socket.fileno(), actor.process_time()))
+                if actor.process_time() > 1000:
+                    self.poll.unregister(fileno)
+                    self.clients[fileno].close(event)
+                    remove.append(fileno)
+            logging.info('ACTORS\t%s', actors)
+            for fileno in remove:
+                del self.clients[fileno]
